a946979ae93b6fd56116e588033e21d5bcc35566
lavinia-ns <laviniam@nicasource.com>
2024-08-12 16:13:14 -0600

    Header and Layout

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="flex items-center w-screen">
+    <img
+      @click="reload"
+      class="max-w-10"
+      src="@/assets/cashflow-icon.svg"
+      alt="Logo"
+    />
+    <h1 class="text-2xl font-bold pl-16">
+      <span
+        class="bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-sky-500"
+      >
+        CashFlow
+      </span>
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    reload() {
+      location.reload();
+    },
+  },
+};
+</script>
---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex fixed px-6 py-8">
+    <slot name="header"></slot>
+  </div>
+</template>
