7c0476acc0e0ca406ac7400d2d92d17713dc8700
lavinia-ns <laviniam@nicasource.com>
2024-08-18 16:54:18 -0600

    Layout, home, resume and movements

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,17 +3,29 @@
     <template #header>
       <HeaderComponent />
     </template>
+
+    <template #resume>
+      <Resume />
+    </template>
+
+    <template #movements>
+      <Movements />
+    </template>
   </LayoutComponent>
 </template>
 
 <script>
 import LayoutComponent from "./LayoutComponent";
 import HeaderComponent from "./HeaderComponent";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Resume/Movements.vue";
 
 export default {
   components: {
     LayoutComponent,
     HeaderComponent,
+    Resume,
+    Movements,
   },
 };
 </script>
